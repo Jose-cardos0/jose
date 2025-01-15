0c1014ff9591fb471e8c506e15e7c8e58906ec9e
Jose Cardoso <josedeveloperJS@gmail.com>
2025-01-15 17:51:50 -0300

    jose portfolio

---
+++ src/app/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <main className="w-screen ">
+      <header className="max-w-7xl m-auto">
+        <nav className="flex justify-between items-center py-4">
+          <li className="list-none">Sobre</li>
+          <li className="list-none">Portfólio</li>
+          <li className="list-none">Meus Projetos</li>
+        </nav>
+      </header>
+    </main>
+  );
+}
